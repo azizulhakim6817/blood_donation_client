279fc09356ca87b3e50a8c12e06b21ae7b7160dd
azizul6817 <azizulhakim68178@gmail.com>
2026-02-15 20:04:04 +0600

    payment-success-update-and-modifired

--- src/pages/Dashboard/Payments/PaymentSuccess/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payments/PaymentSuccess/PaymentSuccess.jsx
@@ -13,17 +13,18 @@ const PaymentSuccess = () => {
         <h1 className="text-3xl font-bold text-green-700 mb-2">
           Payment Successful!
         </h1>
-        <p className="text-gray-600 mb-4">
+        <p className="text-primary font-bold mb-4">
           Thank you for your generous donation.
         </p>
         {sessionId && (
           <p className="text-gray-500 text-sm break-all">
-            Your session ID: <span className="font-mono">{sessionId}</span>
+            <span className="font-bold">Your session ID </span>:{" "}
+            <span className="font-mono">{sessionId}</span>
           </p>
         )}
         <button
           onClick={() => (window.location.href = "/dashboard")}
-          className="mt-6 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-6 rounded-lg transition duration-200"
+          className="mt-6 bg-primary hover:bg-accent text-white font-semibold py-2 px-6 rounded-lg transition duration-200"
         >
           Back to Dashboard
         </button>
